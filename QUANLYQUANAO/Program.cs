8548d9f651feebd67cea95cb19407f517834b932
TrungNhan081203 <nhanbtt21@uef.edu.vn>
2023-10-06 15:10:01 +0700

    moi cap nhat from nhân viên

--- QUANLYQUANAO/Program.cs
+++ QUANLYQUANAO/Program.cs
@@ -16,7 +16,7 @@ namespace QUANLYQUANAO
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new frmHoaDonBan());
+            Application.Run(new frmNhanVien());
         }
     }
 }
--- QUANLYQUANAO/Properties/Resources.Designer.cs
+++ QUANLYQUANAO/Properties/Resources.Designer.cs
@@ -83,6 +83,26 @@ namespace QUANLYQUANAO.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap icon_bo_qua {
+            get {
+                object obj = ResourceManager.GetObject("icon bo qua", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap iconsua {
+            get {
+                object obj = ResourceManager.GetObject("iconsua", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap lưu {
             get {
                 object obj = ResourceManager.GetObject("lưu", resourceCulture);
