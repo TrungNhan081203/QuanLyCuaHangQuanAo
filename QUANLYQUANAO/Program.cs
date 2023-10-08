f2212d4a9bafd0a68cbdecd6f12728342a6e8789
Hoàng Vũ <95397725+danghoangvu03@users.noreply.github.com>
2023-10-08 14:17:49 +0700

    Testgit

--- QUANLYQUANAO/Program.cs
+++ QUANLYQUANAO/Program.cs
@@ -16,7 +16,7 @@ namespace QUANLYQUANAO
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new frmNhanVien());
+            Application.Run(new frmDMKhachHang());
         }
     }
 }
